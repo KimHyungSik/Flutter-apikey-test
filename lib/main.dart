2752599f4d8b5120db69332f572041d986250644
KimHyungSik <colrog968@gmail.com>
2023-10-29 19:35:11 +0900

    api key test

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:api_key_test/api_key.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -9,10 +12,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [Text("data")],
+    return const Scaffold(
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "data",
+            ),
+            Text(
+              gitignoreKey,
+            ),
+            Text(
+              String.fromEnvironment(
+                "complie_test_key",
+                defaultValue: "NotFound",
+              ),
+            ),
+            Text(
+              String.fromEnvironment(
+                "test_json_key",
+                defaultValue: "NotFound",
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
